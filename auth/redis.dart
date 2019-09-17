d2d6faf9eb40a128131ed769eaf3e6a54a934a3e
Iman Daneshi <emandaneshikohan@gmail.com>
2019-09-17 16:51:14 +0430

    Use SADD to list user's tokens

--- auth/redis.dart
+++ auth/redis.dart
@@ -22,12 +22,13 @@ class Redis {
     }
   }
 
-  bool SetToken(String token, String userId) {
+  Future<bool> SetToken(String token, String userId) async {
     RedisConnection conn = new RedisConnection();
     bool tokenSet = false;
-    conn.connect(this.redisHost, this.redisPort).then((Command command) {
-      command.send_object(["SET", token, userId]).then((var response) {
+    await conn.connect(this.redisHost, this.redisPort).then((Command command) async {
+      await command.send_object(["SET", token, userId]).then((var response) async {
         tokenSet = response == 'OK';
+        await command.send_object(["SADD", "user_tokens:" + userId, token]);
       });
     });
     return tokenSet;
@@ -37,8 +38,13 @@ class Redis {
     RedisConnection conn = new RedisConnection();
     bool tokenDeleted = false;
     await conn.connect(this.redisHost, this.redisPort).then((Command command) async {
-      await command.send_object(["DEL", token]).then((var response) {
-        tokenDeleted = response == 1;
+      await command.send_object(["GET", token]).then((var userId) async {
+        if (userId != null){
+          await command.send_object(["DEL", token]).then((var response) async {
+            tokenDeleted = response == 1;
+            await command.send_object(["SREM", "user_tokens:" + userId, token]);
+          });
+        }
       });
     });
     return tokenDeleted;
